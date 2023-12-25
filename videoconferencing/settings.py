ce805bbabf37b1dd5c8956f60dab583d600b3861
DAMODAR TIWARI <117446972+damodar-ai@users.noreply.github.com>
2023-12-25 15:15:55 +0530

    Update settings.py

--- videoconferencing/settings.py
+++ videoconferencing/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6fdgh7tki&^y_-cl4%tpyl-$kh9($vos&a5q&uq%!az2gs__bz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
